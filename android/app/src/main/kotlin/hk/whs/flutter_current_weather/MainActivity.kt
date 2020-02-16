bcdbf23376fcedb8af67d7944dceca36d85c149f
whs <hswongac@gmail.com>
2020-02-16 16:43:20 +0800

    First commit

---
+++ android/app/src/main/kotlin/hk/whs/flutter_current_weather/MainActivity.kt
@@ -0,0 +1,12 @@
+package hk.whs.flutter_current_weather
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
